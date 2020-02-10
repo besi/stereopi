f04c64c51451f2ec47ac55a668a3625cc3efb4bf
Beat Besmer <beat.besmer@gmail.com>
2020-02-10 23:20:26 +0100

    Start stereopi at boot

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 # This needs to be run from the root of the project
 
@@ -7,6 +8,11 @@ pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 
 
 # Disable the popping of the speaker (We don't have an I2S mic so it's fine)
-cp dist/etc/systemd/system/aplay.service /etc/systemd/system/aplay.service
+cp dist/etc/systemd/system/aplay.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable aplay
+
+# Startup the stereopi at boot
+cp dist/etc/systemd/system/stereopi.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable stereopi
