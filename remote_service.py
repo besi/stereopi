31c35f3d527cc69d854463d051eb2910752dec1b
Beat Besmer <beat.besmer@gmail.com>
2020-02-11 00:29:02 +0100

    Allow pressing the color buttons on the remote

---
+++ remote_service.py
@@ -0,0 +1,43 @@
+import evdev
+
+
+class RemoteService(object):
+    __instance = None
+
+    def __new__(cls):
+        # Singleton initializer
+        if RemoteService.__instance is None:
+            RemoteService.__instance = object.__new__(cls)
+        return RemoteService.__instance
+
+    is_listening = False
+
+    def start_listening(self, on_key_pressed):
+
+        device = evdev.InputDevice('/dev/input/event1')
+        if 'lirc' in device.name:
+            device = evdev.InputDevice('/dev/input/event0')
+        print(device)
+
+        down = 1
+        up = 0
+        hold = 2
+
+        RemoteService.is_listening = True
+
+        for event in device.read_loop():
+
+            if not RemoteService.is_listening: break
+            if event.type == evdev.ecodes.EV_KEY and event.value == down:
+                # print(evdev.categorize(event))
+                key = evdev.ecodes.KEY[event.code]
+
+                # The mute key is special since it will produce an array of keys
+                if event.code == 113:
+                    key = 'KEY_MUTE'
+
+                print(key)
+                on_key_pressed(key)
+
+    def stop_listener(self):
+        RemoteService.is_listening = False
--- stereopi.py
+++ stereopi.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3 -u
 
+import math
 import time
+
+import RPi.GPIO as GPIO
 import board
 import neopixel
-import math
-import RPi.GPIO as GPIO
 
-pixels = neopixel.NeoPixel(board.D18,1)
+pixels = neopixel.NeoPixel(board.D18, 1)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Read input pin 12 which is the switch
 
@@ -20,15 +20,36 @@ increment = 0.1
 sleep = 0.01
 dimmer = .04
 
+from remote_service import RemoteService
+
+
+def on_key_pressed(key):
+    print(key)
+    if key == 'KEY_RED':
+        pixels[0] = (int(255 * dimmer), 0, 0)
+    elif key == 'KEY_GREEN':
+        pixels[0] = (0, int(255 * dimmer), 0)
+    elif key == 'KEY_YELLOW':
+        pixels[0] = (int(255 * dimmer), int(255 * dimmer), 0)
+    elif key == 'KEY_BLUE':
+        pixels[0] = (0, 0, int(255 * dimmer))
+    elif key == 'KEY_PLAYPAUSE':
+        pixels[0] = (0, 0, 0)
+    else:
+        pixels[0] = (int(255 * dimmer), 0, int(255 * dimmer))
+
+
+service = RemoteService()
+service.start_listening(on_key_pressed)
+
 while True:
 
-  if GPIO.input(12) == 0:
-     dimmer = 1 - dimmer
-     time.sleep(.2)
-  blue = abs(int(math.sin(x)*255*dimmer))
-  red = abs(int(math.cos(x)*255*dimmer))
-  # green = abs(int(math.cos(x + math.pi/4)*255*dimmer))
-  pixels[0] = (red,0,blue)
-  x = x + increment
-  time.sleep(sleep)
-
+    if GPIO.input(12) == 0:
+        dimmer = 1 - dimmer
+        time.sleep(.2)
+    blue = abs(int(math.sin(x) * 255 * dimmer))
+    red = abs(int(math.cos(x) * 255 * dimmer))
+    # green = abs(int(math.cos(x + math.pi/4)*255*dimmer))
+    pixels[0] = (red, 0, blue)
+    x = x + increment
+    time.sleep(sleep)
