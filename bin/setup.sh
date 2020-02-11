b1293684dc9c304ce050b1201ee433c5dd9379a0
Beat Besmer <beat.besmer@gmail.com>
2020-02-11 18:01:42 +0100

    Start the tuner at boot

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,7 +6,6 @@ pip3 install paho-mqtt
 pip3 install evdev
 pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 
-
 # Disable the popping of the speaker (We don't have an I2S mic so it's fine)
 sudo cp dist/etc/systemd/system/aplay.service /etc/systemd/system/
 sudo systemctl daemon-reload
@@ -16,3 +15,8 @@ sudo systemctl enable aplay
 sudo cp dist/etc/systemd/system/stereopi.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable stereopi
+
+# Startup the radio at boot
+sudo cp dist/etc/systemd/system/tuner.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable tuner
