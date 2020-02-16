6a63a92df6459516cf4dfe4911688b679c420590
Beat Besmer <beat.besmer@gmail.com>
2020-02-16 21:17:58 +0100

    Python packages need to be installed with sudo. This is due to the Neopixel: NeoPixel support requires running with sudo

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,9 +2,11 @@
 
 # This needs to be run from the root of the project
 
-pip3 install paho-mqtt
-pip3 install evdev
-pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+#  NeoPixel support requires running with sudo
+sudo pip3 install paho-mqtt
+sudo pip3 install evdev
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+/usr/bin/say installed python packages
 
 # Disable the popping of the speaker (We don't have an I2S mic so it's fine)
 sudo cp dist/etc/systemd/system/aplay.service /etc/systemd/system/
