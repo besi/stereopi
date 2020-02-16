ea7bd835a203ffcf0deeede825a03021049894e5
Beat Besmer <beat.besmer@gmail.com>
2020-02-16 22:06:33 +0100

    Improve test code

--- stereopi.py
+++ stereopi.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3 -u
-
 import math
+import os
 import time
 
 import RPi.GPIO as GPIO
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 1)
+switch_pin = 13
+led_pin = board.D12
+
+pixels = neopixel.NeoPixel(led_pin, 1)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Read input pin 12 which is the switch
+GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Starting...")
 x = 0
@@ -21,7 +22,6 @@ sleep = 0.01
 dimmer = .04
 
 from remote_service import RemoteService
-
 
 def on_key_pressed(key):
     print(key)
@@ -35,12 +35,12 @@ def on_key_pressed(key):
         pixels[0] = (0, 0, int(255 * dimmer))
     elif key == 'KEY_PLAYPAUSE':
         pixels[0] = (0, 0, 0)
-    else:
-        pixels[0] = (int(255 * dimmer), 0, int(255 * dimmer))
     elif key == 'KEY_VOLUMEUP':
         os.system("amixer set PCM 5%+")
     elif key == 'KEY_VOLUMEDOWN':
         os.system("amixer set PCM 5%-")
+    else:
+        pixels[0] = (int(255 * dimmer), 0, int(255 * dimmer))
 
 
 service = RemoteService()
@@ -48,7 +48,7 @@ service.start_listening(on_key_pressed)
 
 while True:
 
-    if GPIO.input(12) == 0:
+    if GPIO.input(switch_pin) == 0:
         dimmer = 1 - dimmer
         time.sleep(.2)
     blue = abs(int(math.sin(x) * 255 * dimmer))
