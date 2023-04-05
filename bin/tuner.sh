34aab041c3bc42fc5a0eae54b9b7510bcc61686c
Beat Besmer <beat.besmer@gmail.com>
2023-04-05 21:44:45 +0200

    Use the main mix of radioparadise in the tuner

--- bin/tuner.sh
+++ bin/tuner.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 /usr/bin/amixer set PCM 20%
 
-URL=http://stream.radioparadise.com/mellow-192
+URL=http://stream.radioparadise.com/mellow-128
+URL=http://stream.radioparadise.com
 /usr/bin/mplayer $URL -cache 192 -nolirc -prefer-ipv4 -really-quiet -slave
