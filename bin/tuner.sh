d5c36ef7345451fd5d1da14b7a28b50d799649d4
Beat Besmer <beat.besmer@gmail.com>
2026-01-19 17:09:48 +0100

    list additional urls for radioparadise

--- bin/tuner.sh
+++ bin/tuner.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 /usr/bin/amixer set PCM 20%
 
+URL=https://stream.radioparadise.com/rock-flac
+
 URL=http://stream.radioparadise.com/mellow-128
+
 URL=http://stream.radioparadise.com
+URL=http://stream-uk1.radioparadise.com/aac-128
+URL=http://stream-uk1.radioparadise.com/aac-320
+URL=https://stream.radioparadise.com/flac
+
 /usr/bin/mplayer $URL -cache 192 -nolirc -prefer-ipv4 -really-quiet -slave
